4b9b7dd66657795d77ad115a37f7de482851c111
Maxim <maxim@kryzh.ru>
2023-02-25 18:25:12 +0300

    GetNumberFromUser function added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
